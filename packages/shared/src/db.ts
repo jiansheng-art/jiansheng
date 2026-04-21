0160e5a024b16a1f335ede2cc1d93e06480c6bdd
ZTL-UwU <zhangtianli2006@gmail.com>
2026-04-20 17:49:27 -0700

    fix: use neon websocket driver
    Signed-off-by: ZTL-UwU <zhangtianli2006@gmail.com>

--- packages/shared/src/db.ts
+++ packages/shared/src/db.ts
@@ -1,10 +1,12 @@
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 import { env } from './env';
 import * as schema from './schema';
+import ws from 'ws';
 
 export const db = drizzle({
   connection: {
     connectionString: env.DATABASE_URL,
   },
   schema,
+  ws,
 });
